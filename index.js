4efb812c2c78be929cb79ea652217146cdd074b0
zidi <zidi01@ukr.net>
2018-04-30 11:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
-  recipes[prop2] = '2';
+  recipes['prop2'] = '2';
   return recipes;
 }
 
