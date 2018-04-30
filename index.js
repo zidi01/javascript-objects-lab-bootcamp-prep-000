399a5583849cb0c14564a2ab499c47ffa27ad530
zidi <zidi01@ukr.net>
2018-04-30 11:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var newRecipes = Object.assign(recipes);
 
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {['prop2']: "2"});
+  // in previously lab, the "{}" was failed
+  // now it is okay. Why?
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
