cad9b156afc23a4a32a483809ddbfa4c6d9bc79b
zidi <zidi01@ukr.net>
2018-04-30 11:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
- delete recipes.prop;
+ delete recipes['prop'];
  return recipes;
 }
 
