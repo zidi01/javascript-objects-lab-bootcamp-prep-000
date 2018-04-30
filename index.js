feccfb368e2d9ee9258a91751e0baef6bafcbc5b
zidi <zidi01@ukr.net>
2018-04-30 11:12:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var recipes = {prop: 1};
 var newRecipes = Object.assign(recipes);
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(recipes, {prop2: '2'});
+function updateObjectWithKeyAndValue(newRecipes, key, value) {
+  Object.assign(recipes, {prop2: '2'});
+  return 
   
 }
 
